8c94cae08c14c1dad0fed0a4b237963de9cf822c
Sukanya Sengupta <sukanyassg0410@gmail.com>
2023-03-05 20:41:33 +0530

    copied file from dir1 to dir2 and updated the raw code

---
+++ dir2/copy_addition.cs
@@ -0,0 +1,17 @@
+class add
+{
+    static void Main(string[] args)
+    {
+        int a, b;
+        Console.WriteLine("enter 1st num= ");
+        a=int.Parse(Console.ReadLine());
+        Console.WriteLine("enter 2nd num= ");
+        b=int.Parse(Console.ReadLine());
+        
+        int result= a+b;
+        
+        Console.WriteLine("Addition is: "+result);
+        Console.WriteLine("Hello world!!");
+	Console.ReadKey();
+    }
+}
